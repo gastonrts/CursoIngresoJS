dd96152e4b889ad85536e242a47583f109389d8f
gastonrts <40776563+gastonrts@users.noreply.github.com>
2018-07-05 21:55:34 -0300

    Ejercicio uno y tres  TERMINAR

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -1,4 +1,31 @@
 function mostrar()
 {
 
+	var precio;
+	var descuento;
+	var precioDescuento;
+	var precioConDescuento;
+	
+	precio = prompt ('Ingrese el precio');
+	precio = parseInt (precio);
+
+	descuento = prompt ('Ingrese el % de descuento');
+	descuento = parseInt (descuento);
+
+	precioDescuento = precio * descuento / 100;
+	precioConDescuento = precio * descuento / 100 + precio;
+	
+
+
+
+
+	alert ("El descuento en dinero es" + preciodescuento + ', el precio con descuento es' + precioConDescuento +);
+
+
+
+
+
+
+
+
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,22 @@
 
 function mostrar()
 {
+	var base;
+	var altura;
+	var perimetro;
+	var superficie; 
+	
+	base = prompt ("Ingrese base del triangulo"); 
+	base = parseInt (base);
 
-}
+	altura = prompt ("Ingrese altura del triangulo");
+	altura = parseInt (altura);
+
+	perimetro = base + base + base;
+	superficie = base * altura / 2;
+
+	alert ("El perimetro del triangulo es... " + perimetro + ' y su superficie es...' + superficie  )
+
+
+
+	}
