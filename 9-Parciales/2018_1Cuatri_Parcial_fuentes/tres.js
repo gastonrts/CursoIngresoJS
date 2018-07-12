5ff90db33d93b0bf3847d818a3aeb0e3d6d45a82
gastonrts <40776563+gastonrts@users.noreply.github.com>
2018-07-12 14:25:19 -0300

    EJERCICIO 1 Y 3 DEL PARCIAL LISTOS

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,18 @@
 function mostrar()
 {
+	var precio;
+	var descuento;
+	var preciofinal;
+
+	precio = prompt ('Ingrese el precio');
+	descuento = prompt ('Ingrese el % de descuento');
+
+	precio = parseInt (precio);
+	descuento = parseInt (descuento);
+
+	descuento = (precio * descuento / 100);
+	preciofinal = precio - descuento;
+
+	document.getElementById ('elPrecioFinal').value=preciofinal;
 
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -2,4 +2,19 @@
 function mostrar()
 {
 
+		var largo;
+		var ancho;
+		var perimetro;
+
+
+		largo = prompt ("Ingrese largo de un rectangulo");
+		largo = parseInt (largo);
+		ancho = prompt ("Ingrese ancho de un rectangulo");
+		ancho = parseInt (ancho);
+
+
+		perimetro = (2 * (ancho + largo));
+
+		alert ('El perimetro es ' + perimetro);
+
 }
