126473eb8dc44750cb28987d54325d4e16960cba
gastonrts <40776563+gastonrts@users.noreply.github.com>
2018-07-12 20:35:13 -0300

    ejercicio 1

--- 2-InstruccionIf/jsInstruccionIF (1).js
+++ 2-InstruccionIf/jsInstruccionIF (1).js
@@ -1,7 +1,25 @@
 function mostrar()
+
+	//2 POSIBLES VALORES 'TRUE' O 'FALSE' 
+				// LAS ESTRUCTURAS IF NO LLEVAN PUNTO Y COMA (;)
+				//OPERADORES DE COMBINACION == Es IGUALDAD 'Si son iguales...' ejem, (edad == 18).
+				// SIGNO: != ESTO NIEGA LO QUE ESTÁ EJEMPLO 
+				// > o >= Mayor o mayor igual, necesitan 2 operandos
+				// < ó <= Menos o menor o igual
+				// === IGUALDAD ESTRICTA EJEMPLO: Boolean 2 === "2" String, compara el valor.
+				// == No es lo mismo que ===. 
+				//Operadores Lógicos : && Y || ejemplo (edad==18  || sexo== 'M') 
+				// Cuando es Y (&&): SOLO CUANDO LAS DOS SON VERDADERAS ES 'TRUE'. Sino, es falsa.
+				// Cuando es O(||): Solo es 'FALSE' cuando los dos son 'False'. Sino, siempre es verdadera ('True');
+				// POR CADA IF, SOLO HAY UN ELSE Y SE ESCRIBE FUERA DE {}.	
+
+
 {
-//tomo la edad  
-
-
+ var edad;
+ document.getElementById('edad') ; 
+ 	if (edad==15)
+ {alert ('Niña bonita');
+ 
+ }
 
 }//FIN DE LA FUNCIÓN
