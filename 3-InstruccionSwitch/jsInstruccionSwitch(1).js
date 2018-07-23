b035417e794d1c4772dbeea8e69e9e944aa19544
gastonrts <40776563+gastonrts@users.noreply.github.com>
2018-07-23 15:57:22 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -1,7 +1,32 @@
 function mostrar()
 {
-//tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+	var mesDelAño = document.getElementById('mes').value;
+
+	switch (mesDelAño)
+
+	{
+		case 'Enero':
+			alert ('Es verano')
+			break;
+		
+
+		case 'Marzo':
+			alert ('A clases')
+			break;
+		
+		
+		case 'Julio':	
+			alert ('Se vienen las vacaciones')
+			break;
+		
+		case 'Diciembre':
+			alert ('Felices Fiestas')
+			break;	
+		
+
+
+
+	}
 
 
 
