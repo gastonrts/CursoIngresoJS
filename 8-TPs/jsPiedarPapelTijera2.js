ed78a2bee21e5e3099f28d463776003fce323cd1
gastonrts <40776563+gastonrts@users.noreply.github.com>
2018-07-23 16:11:33 -0300

    Add files via upload

--- 8-TPs/jsPiedarPapelTijera2.js
+++ 8-TPs/jsPiedarPapelTijera2.js
@@ -1,31 +1,86 @@
-var eleccionMaquina;
-var ContadorDeEmpates=0;
-var ContadorDeGanadas=0;
-var ContadorDePerdidas=0;
+	var eleccionMaquina;
+	var ContadorDeEmpates=0;
+	var ContadorDeGanadas=0;
+	var ContadorDePerdidas=0;
+
 
 function comenzar()
 {
-	//Genero el número RANDOM entre 1 y 3
-	 	
+
+
+	eleccionMaquina=	Math.floor (Math.random() * (4-1)+1);	 	
 
 
 
-}//FIN DE LA FUNCIÓN
+}
+//FIN DE LA FUNCIÓN
 function piedra()
 {
+	if (eleccionMaquina==1)
+	{
+		ContadorDeEmpates++;
+	}
+		else if (eleccionMaquina==2)
+		{
+			ContadorDePerdidas++;
+		}
+		else if (eleccionMaquina==3)
+		{
+			ContadorDeGanadas++;
+		}
+			document.getElementById('empatadas').value=ContadorDeEmpates;
+			document.getElementById('ganadas').value=ContadorDeGanadas;
+			document.getElementById('perdidas').value=ContadorDePerdidas;
 	
 
 }//FIN DE LA FUNCIÓN
 function papel()
 {
 
+	if (eleccionMaquina==2) //Si la maquina elije papel...
+	{	
+	ContadorDeEmpates++;
+	}
+		else if (eleccionMaquina==1) //Si elije piedra...
+		{
+			ContadorDeGanadas++;
+		}
+		else if (eleccionMaquina==3)//Si elije tijera...
+		{
+			ContadorDePerdidas++;
+		}
+			document.getElementById('empatadas').value=ContadorDeEmpates;
+			document.getElementById('ganadas').value=ContadorDeGanadas;
+			document.getElementById('perdidas').value=ContadorDePerdidas;
 }//FIN DE LA FUNCIÓN
 function tijera()
 {
 	
+	if (eleccionMaquina==3)
+	{
+		ContadorDeEmpates++;
+
+	}
+		else if (eleccionMaquina==2)
+		{
+			ContadorDeGanadas++;
+		}
+		else if (eleccionMaquina==1)
+		{
+			ContadorDePerdidas++;
+		}
+			document.getElementById('empatadas').value=ContadorDeEmpates;
+			document.getElementById('ganadas').value=ContadorDeGanadas;
+			document.getElementById('perdidas').value=ContadorDePerdidas;
+
+
+
 }//FIN DE LA FUNCIÓN
 
 function mostrarResultado()
 {
 
+
+
+	
 }
