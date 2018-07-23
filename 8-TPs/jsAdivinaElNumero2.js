d0f0847b3de0fdd42975d752c81512255bcc54bd
gastonrts <40776563+gastonrts@users.noreply.github.com>
2018-07-23 15:58:58 -0300

    Add files via upload

--- 8-TPs/jsAdivinaElNumero2.js
+++ 8-TPs/jsAdivinaElNumero2.js
@@ -10,79 +10,58 @@ Desde  6 Intentos hasta 10:”falta técnica”
 Más de 10 intentos: “afortunado en el amor!!”.*/
 
 var numeroSecreto; 
+
+var numero;
+
 var contadorIntentos;
-contadorIntentos = 1;
-
-
-
-
-
-
+	contadorIntentos = 1;
 
 function comenzar()
-{
+{	
+	alert ('Adiviná el numero secreto (1-10) en menos de 10 intentos')
 	numeroSecreto=	Math.floor (Math.random() * (10-1)+1);	
+	console.log (numeroSecreto);
 }
 
 function verificar()
-{
+{	
+
 	numero = document.getElementById('numero').value;
 
 	document.getElementById('intentos').value = contadorIntentos;
 
-
-	var contadorIntentos = 1;
-	
-		while (contadorIntentos < 10) {
-  		
-			if (numero==numeroSecreto&&contadorIntentos==1)
-			{	
-				alert ('Usted es un psíquico');
-
-			}
-				else if (numero<numeroSecreto&&contadorIntentos==2)
-			{
-				alert ('“Excelente percepción”');
-			}
-				else if (numero>numeroSecreto&&contadorIntentos==3)
-			{
-				alert ('Esto es suerte');
-			}
-				else if (numero>numeroSecreto&&contadorIntentos==4)
-			{
-				alert ('“Excelente técnica');
-			}
-				else if (numero>numeroSecreto&&contadorIntentos==5)
-			{
-				alert ('Usted está en la media');
-			}
-				else if (numero>numeroSecreto&&(contadorIntentos>=6&&contadorIntentos<=10))
-			{
-				alert ('Falta práctica');
-			}
-				else if (contadorIntentos>10)
-			{
-				alert ('Será en otra oportunidad');
-			}
-
-
-
-
-
-
-
-
-
-
-
-
-
-								}
-
-
-
-
-	
-
+		if (numero==numeroSecreto&&contadorIntentos==1)
+		{
+			alert ('Usted es un psiquico');
+		}
+		else if (numero==numeroSecreto&&contadorIntentos==2)
+		{
+			alert ('Excelente percepción');
+		}
+		else if (numero==numeroSecreto&&contadorIntentos==3)
+		{
+			alert ('Esto es suerte');
+		}
+		else if (numero==numeroSecreto&&contadorIntentos==4)
+		{
+			alert ('Excelente técnica');
+		}
+		else if (numero==numeroSecreto&&contadorIntentos==5)
+		{
+			alert ('Está en la media');
+		}
+		else if (numero==numeroSecreto&&(contadorIntentos>=6||contadorIntentos<=10))
+		{
+			alert ('Fué casualidad');
+		}
+		else if (numero==numeroSecreto&&contadorIntentos>=10)
+		{
+			alert ('Perdiste');
+		}
+		else 	(numero!=numeroSecreto&&contadorIntentos<10)
+		{
+			contadorIntentos++;
+		}
 }
+
 
