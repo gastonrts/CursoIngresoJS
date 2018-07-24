5b0faa900790942a13619d685027ad7ec42ad84d
gastonrts <40776563+gastonrts@users.noreply.github.com>
2018-07-24 16:25:43 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,49 @@
 function mostrar()
 {
+	var planetas;
 
+		planetas = prompt ('Ingrese un planeta. Debe estar escrito en minusculas.');
+
+		if (planetas==='tierra')
+		{
+			alert ('Acá vivimos');
+		}
+
+		else if (planetas==='venus'||planetas==='mercurio')
+		{
+			alert ('Acá hace más calor');
+		}
+		else if (planetas==='jupiter'||planetas==='saturno'||planetas==='urano'||planetas=='neptuno')
+		{
+			alert ('Acá hace mas frio');
+		}
+
+		else 
+		{
+			alert (planetas + ' NO ES UN PLANETA VALIDO');
+		}
 }
+/*
+	var planetas;
+
+	planetas = prompt ('Ingrese un planeta que esté dentro del sistema solar. Debe estar escrito en minusculas.');
+		switch (planetas)
+		{
+			case 'tierra':
+				alert ('Acá vivimos')
+				break;
+			case 'venus':
+			case 'mercurio':
+				alert ('Acá hace más calor')
+				break;
+			case'jupiter':
+			case 'saturno':
+			case 'urano':
+			case'neptuno':
+				alert ('Acá hace más frio')
+				break;
+			default:
+			alert ('No es un planeta valido')
+			break;
+		}
+*/
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,40 @@
 function mostrar()
-{
+{	
+	
+	var numeroUno;
+	var numoroDos;
+	var resultado;
+
+	numeroUno = prompt ('Ingrese un número');
+	numoroDos = prompt ('Ingrese un número');
+
+	if (numeroUno==numoroDos)
+	{
+		resultado = numeroUno + numoroDos;
+		alert ('Su número es ' + resultado)
+	}
+
+		else if (numeroUno>numoroDos)
+	{
+		numoroUno =	parseInt (numeroUno);
+		numoroDos = parseInt (numoroDos);
+		resultado = numoroUno - numoroDos;
+		alert ('Su número es ' + resultado);
+	}
+
+		else 
+	{
+		numeroUno = parseInt (numeroUno);
+		numoroDos = parseInt (numoroDos);
+		resultado = numeroUno + numoroDos;
+		alert ('Su número es ' + resultado);
+
+		if (resultado>10)
+		{
+		alert ('La suma es ' + resultado +  ' y supero a 10')
+		}
+	}
+
+	
 
 }
