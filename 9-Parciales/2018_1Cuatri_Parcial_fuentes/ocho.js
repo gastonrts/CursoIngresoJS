51b8c63b9ceaafafc1a1b82d4f0465bfe604a21e
gastonrts <40776563+gastonrts@users.noreply.github.com>
2018-08-02 22:09:38 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,4 +1,15 @@
 function mostrar()
 {
 
+
 }
+/*
+Mara 	F 15 7 
+Jose 	M 18 2
+Fer  	F 25 5
+Susana 	F 65 9 
+MIRTA	F 99 8
+
+1) nombre peor nota = jose
+2) sexo sexo nota mas baja = F
+3) edad y sexo mejor nota = 65 f
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -29,7 +29,7 @@ function mostrar()
 	var acumuladorMujeres = 0;
 	var acumuladorHombres = 0;
 
-	var masViejo;
+	var masViejo; 
 	var masJoven;
 	var sexoMasViejo;
 	var mujerMasVieja;
@@ -37,23 +37,23 @@ function mostrar()
 	while (contador<4)
 	{	
 
-		nombre = prompt ('Ingrese su nombre' + contador);
+			nombre = prompt ('Ingrese su nombre ' + contador);
 
-		sexo = prompt ('Ingres su sexo F para femenino y M para masculino' + contador);
+			sexo = prompt ('Ingres su sexo F para femenino y M para masculino ' + contador);
 
 		while (sexo!='m'&&sexo!='f')
 		{
 			alert ('Ingresó un sexo erroneó');
-			sexo = prompt ('Ingres su sexo F para femenino y M para masculino' + contador);
+			sexo = prompt ('Ingres su sexo F para femenino y M para masculino ' + contador);
 		}
 
-		edad   = prompt ('Ingrese su edad. Entre 0 y 90 años inclusive' + contador);
-		edad   = parseInt (edad);
+			edad   = prompt ('Ingrese su edad. Entre 0 y 90 años inclusive ' + contador);
+			edad   = parseInt (edad);
  
 		while (isNaN(edad)||edad<0||edad>90)
 		{
 			alert ('Ingresó una edad erronea')
-			edad   = prompt ('Ingrese su edad. Entre 0 y 90 años inclusive' + contador);
+			edad   = prompt ('Ingrese su edad. Entre 0 y 90 años inclusive ' + contador);
 			edad   = parseInt (edad);
 
 		}
@@ -81,30 +81,37 @@ function mostrar()
 			contadorMenores++;
 			acumuladorEdades+= edad;
 		}
+
 		//bandera para sacar la edad mas alta y la mas baja
 			if (contadorEdades==0)
 		{	
 			contadorEdades++;
 			edadMasAlta = edad;
 			edadMasBaja = edad;
+			masViejo 	= nombre;
 		}
 			
-			if (edad>edadMasAlta)
-		{
-			edadMasAlta = edad; //la edad mas alta
-			masViejo = nombre; // el nombre del mas viejo
-			sexoMasViejo = sexo; // el sexo del mas viejo
-			if (sexo=='f')
+			else 
+		{	
+			if (edadMasAlta>edad)
+			{			
+				edadMasAlta = edad; //la edad mas alta
+				masViejo = nombre; // el nombre del mas viejo
+				sexoMasViejo = sexo; // el sexo del mas viejo}
+				if (sexo=='f')
 			{
 				mujerMasVieja = nombre; // lo meto adentro para sacar el nombre del mas viejo
 			}
-		}
-			if (edad<edadMasBaja)
+		}		
+				if (edadMasBaja<edad)
 		{
-			edadMasBaja = edad; // la edad mas baja
- 			masJoven	= nombre; // el nombre del mas joven
+				edadMasBaja = edad; // la edad mas baja
+ 				masJoven	= nombre; // el nombre del mas joven
 
 		}
+
+		}
+			
 		//Acá termina la  bandera//
 
 		// saco la edad mas baja//
@@ -113,26 +120,14 @@ function mostrar()
 		{
 			acumuladorMujeres+= edad;
 		}
-		if (edad>0&&edad<90&&sexo=='m')
+			else // cambie if x else. Probar si funciona.		if (edad>0&&edad<90&&sexo=='m')
+
 		{
 			acumuladorHombres+=	edad;
 		}
 		// saco la edad mas baja//
 
-
-		
-
-
-
-
-
-
-
-
-
-
-
-				contador++;
+		contador++;
 		}
 		
 		//punto 1, 2, 3 y 4 //
@@ -140,47 +135,29 @@ function mostrar()
 
 
 		PromedioEdades = acumuladorEdades / contador;
-		promedioEdadMasculino 	= acumuladorMujeres / contadorHombres;
-		promedioEdadFemenino 	= acumuladorHombres / contadorHombres;
+		promedioEdadFemenino	= acumuladorMujeres / contadorMujeres;
+		promedioEdadMasculino 	= acumuladorHombres / contadorHombres;
+						// CONTADOR MAYORES&MENORES - CONTADORES H&M
 		document.write('La cantidad de hombres  es ' + contadorHombres + '<br>');
 		document.write ('La cantidad de mujeres es ' + contadorMujeres + '<br>');
 		document.write ('La cantidad de menores es ' + contadorMenores + '<br>');
 		document.write ('La cantidad de mayores es ' + contadorMayores + '<br>');
-				
+						//EDADES MAS BAJAS//
 		document.write ('La edad mas baja es ' + edadMasBaja + '<br>');
 		document.write ('La edad mas alta es ' + edadMasAlta + '<br>');
+						// PROMEDIOS
 		document.write ('El promedio de edades ' + PromedioEdades + '<br>' );
-
 		document.write ('El promedio de edad de mujeres es ' + promedioEdadFemenino + '<br>' );
 		document.write ('El promedio de edad de hombres es ' + promedioEdadMasculino + '<br>' );
-
+						// NOMBRE DEL MAS VIEJO/JOVEN
 		document.write ('El nombre del más viejo es ' + masViejo + '<br>' );
 		document.write ('El nombre del más joven es ' + masJoven + '<br>' );
+						// SEXO Y NOMBRE DEL MAS VIEJO
 		document.write ('El sexo del más viejo es ' + sexoMasViejo + '<br>' );
 		document.write ('La Mujer más vieja es ' + mujerMasVieja + '<br>' );
 
 
-
-
-
-
-
-		
-
-
-
-
-
 	}
-
-
-
-
-
-
-
-
-
 
 
 
@@ -189,26 +166,27 @@ function mostrar()
 	un document.write por cada dato que quiero mostrar.
 
 
-	1) 	cantidad de mujeres = 2	 		✓
-	2) 	cantidad de hombres = 2			✓
-	3) 	cantidad mayores de edad = 3	✓
-	4) 	cantidad menores de edad = 1	✓
+	1) 	cantidad de mujeres = 2	 			✓
+	2) 	cantidad de hombres = 2				✓
+	3) 	cantidad mayores de edad = 3		✓
+	4) 	cantidad menores de edad = 1		✓
 
 	//Hasta acá va bien//
 
 
-	5) 	la edad mas baja = 16			✓
-	6) 	la edad mas alta = 81			✓
-	7) 	promedio de edad mujeres = 48	✓
-	8) 	promedio de edad hombres = 29	✓
-	8) 	promedio de edad total = 38 	✓
+	5) 	la edad mas baja = 16				✓
+	6) 	la edad mas alta = 81				✓
+	7) 	promedio de edad mujeres = 48		✓
+	8) 	promedio de edad hombres = 29		✓
+	8) 	promedio de edad total = 38 		✓
 
-	10)	nombre del más viejo = Fer 		✓
-	11) nombre del más joven = Maria	✓
-	12) sexo del más viejo = F
-	13) nombre de la mujer mas vieja = Fer
+	10)	nombre del más viejo = Fer 			✓
+	11) nombre del más joven = Maria		✓
+	12) sexo del más viejo = F 				✓
+	13) nombre de la mujer mas vieja = Fer 	✓
 
 */
+
 
 
 
@@ -218,45 +196,25 @@ function mostrar()
 
 
 /*
-var alumnos;
-	var notas;
-	var sexo;
-	var contador = 0;
-	var acumulador = 0;
-	var contadorMujeres = 0;
-	var contadorHombres = 0;
 
-	while (contador<5)
-	{	
-		notas = prompt ('Ingrese su nota');
-		notas = parseInt (notas);
-		sexo  = prompt ('Ingrese su sexo');
+		if (edad>edadMaximaFemenino) //Hacer otro edad maximo para femeninos para que entre SOLO si es mujer.
 
-		
-		contador++;
-		if (IsNaN(notas))
 		{
-			notas = prompt ('Ingrese su nota');
-			notas = parseInt (notas);
-		}
-		while (sexo!='f'&&sexo!='m')
-		{
-			alert ('Ingreso un género invalido.')
-			sexo  = prompt ('Ingrese su sexo');
-		}
-		
-
-		while (notas>0&&notas<10&&(sexo=='f'))
-		{
-			contadorMujeres ++;
-			acumulador += notas;
-		}
-		while (notas>0&&notas<10&&(sexo=='m'))
-		{
-			contadorHombres ++;
-			acumulador += notas;
+			edadMaximaFemenino = edad;
+			nombreFemeninoMasViejo = nombre
 		}
 
-		alert ('Mujeres' + contadorMujeres + 'Hombres' + contadorHombres + 'notas' + notas  );
+		if (contador==0)
 
-*/
+		{
+			maximo = edad:
+		}
+
+		else {
+			if (edad>maximo)
+			{
+				maximoo = edad;
+			}
+		}
+
+*/// for primero ejecuta 'contador = 0' después los demás y cuando vuelve a las condiciones ejecuta asegurandose que el contador sea menor a 5.
